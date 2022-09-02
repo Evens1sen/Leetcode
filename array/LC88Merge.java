4c7c964e076c47e04ab661c43701c5af1fd7491a
mhy <11912919@mail.sustech.edu.cn>
2022-09-02 11:07:39 +0800

    Inplace merge

---
+++ array/LC88Merge.java
@@ -0,0 +1,33 @@
+package array;
+
+import java.util.Arrays;
+
+public class LC88Merge {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        for (int k = m + n - 1; k >= 0; k--) {
+            if (i < 0){
+                nums1[k] = nums2[j];
+                j--;
+            } else if (j < 0) {
+                nums1[k] = nums1[i];
+                i--;
+            }else if (nums1[i] >= nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+        }
+    }
+}
