8616f29a71bd4afd093d2d7194f5cdd695a41a0d
mhy <11912919@mail.sustech.edu.cn>
2022-09-05 20:58:30 +0800

    Monotonic stack problems

---
+++ stack/LC739DailyTemperatures.java
@@ -0,0 +1,29 @@
+package stack;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class LC739DailyTemperatures {
+
+    public static void main(String[] args) {
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
+    }
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Deque<Integer> stack = new LinkedList<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return res;
+    }
+
+}
