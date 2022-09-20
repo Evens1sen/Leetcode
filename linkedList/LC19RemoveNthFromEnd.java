7d4ef855069ccd901780efe3861551d7a75829d3
mhy <11912919@mail.sustech.edu.cn>
2022-09-20 17:18:38 +0800

    More on linked list

---
+++ linkedList/LC19RemoveNthFromEnd.java
@@ -0,0 +1,20 @@
+package linkedList;
+
+public class LC19RemoveNthFromEnd {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode fast = head;
+        ListNode slow = dummy;
+        for (int i = 0; i < n; ++i) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
