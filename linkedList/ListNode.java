7fa07a86bc65a197a7360b4d3567ee4933f7bd5f
mhy <11912919@mail.sustech.edu.cn>
2022-09-22 19:39:16 +0800

    Add string constructor for ListNode

--- linkedList/ListNode.java
+++ linkedList/ListNode.java
@@ -1,5 +1,7 @@
 package linkedList;
 
+import java.util.Arrays;
+
 public class ListNode {
     int val;
     ListNode next;
@@ -12,6 +14,22 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    ListNode(String listStr) {
+        int[] nums = Arrays.stream(listStr.substring(1, listStr.length() - 1).split(","))
+                .map(String::trim).mapToInt(Integer::parseInt).toArray();
+
+        ListNode dummy = new ListNode(0, null);
+        ListNode cur = dummy;
+        for (int num : nums) {
+            cur.next = new ListNode(num, null);
+            cur = cur.next;
+        }
+
+        this.val = dummy.next.val;
+        this.next = dummy.next.next;
+    }
+
 
     public void printList() {
         ListNode cur = this;
