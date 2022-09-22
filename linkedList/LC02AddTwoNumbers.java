e88683dc0111f3d4db97953ea64903e61396e444
mhy <11912919@mail.sustech.edu.cn>
2022-09-22 19:56:55 +0800

    Modify the calculation of carry

--- linkedList/LC02AddTwoNumbers.java
+++ linkedList/LC02AddTwoNumbers.java
@@ -17,12 +17,8 @@ public class LC02AddTwoNumbers {
             int x = (l1 == null ? 0 : l1.val);
             int y = (l2 == null ? 0 : l2.val);
             int sum = x + y + carry;
-            if (sum >= 10) {
-                sum -= 10;
-                carry = 1;
-            } else {
-                carry = 0;
-            }
+            carry = sum / 10;
+            sum = sum % 10;
 
             cur.next = new ListNode(sum, null);
             cur = cur.next;
