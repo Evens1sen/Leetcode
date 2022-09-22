fffe994836f46b4724df3b3a30adea67767dbe5c
mhy <11912919@mail.sustech.edu.cn>
2022-09-22 19:55:39 +0800

    Daily exercise

---
+++ linkedList/LC02AddTwoNumbers.java
@@ -0,0 +1,38 @@
+package linkedList;
+
+public class LC02AddTwoNumbers {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode("[2, 4, 3]");
+        ListNode l2 = new ListNode("[5, 6, 4]");
+        ListNode res = addTwoNumbers(l1, l2);
+        res.printList();
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(0, null);
+        ListNode cur = res;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = (l1 == null ? 0 : l1.val);
+            int y = (l2 == null ? 0 : l2.val);
+            int sum = x + y + carry;
+            if (sum >= 10) {
+                sum -= 10;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+
+            cur.next = new ListNode(sum, null);
+            cur = cur.next;
+            l1 = (l1 == null ? null : l1.next);
+            l2 = (l2 == null ? null : l2.next);
+        }
+
+        if (carry == 1) {
+            cur.next = new ListNode(1, null);
+        }
+        return res.next;
+    }
+}
