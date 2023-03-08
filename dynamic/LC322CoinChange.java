4daf8e6323ff80fc28acb9f67af0348584ff6455
mhy <11912919@mail.sustech.edu.cn>
2023-03-08 16:51:22 +0800

    Review knapsack problems

--- dynamic/LC322CoinChange.java
+++ dynamic/LC322CoinChange.java
@@ -10,16 +10,19 @@ public class LC322CoinChange {
         System.out.println(coinChange(coins, amount));
     }
 
-    // f(n) = min(f(n - coin), f(n))
+
+    // Be aware that this is not a knapsack problem, because we need to find the minimal value
     public static int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1]; // The min number of coins sum to amount i
+        // The minimal number of coins sum to amount i
+        // dp[i] = min(dp[i-coins[j]]) + 1
+        int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
 
         for (int i = 0; i <= amount; i++) {
-            for (int coin : coins) {
-                if (coin <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
--- dynamic/LC518Change.java
+++ dynamic/LC518Change.java
@@ -12,8 +12,11 @@ public class LC518Change {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= amount; j++) {
                 if (j - coins[i - 1] < 0) {
+                    // Pay attention when amount < 0
                     dp[i][j] = dp[i - 1][j];
                 } else {
+                    // Complete knapsack problem:
+                    // Coins are not limited, so dp[i][j-coins[i-1]], not dp[i-1][j-coins[i-1]]
                     dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
                 }
             }
