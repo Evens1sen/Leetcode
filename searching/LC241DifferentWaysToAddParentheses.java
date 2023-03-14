a66fe423de18ccf4412270e41297270937b2543a
mhy <11912919@mail.sustech.edu.cn>
2023-03-14 22:01:34 +0800

    Divide and conquer

---
+++ searching/LC241DifferentWaysToAddParentheses.java
@@ -0,0 +1,38 @@
+package searching;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC241DifferentWaysToAddParentheses {
+
+    public List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> res = new ArrayList<>();
+        if (expression.chars().allMatch(Character::isDigit)) {
+            res.add(Integer.parseInt(expression));
+            return res;
+        }
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            if (ch == '+' || ch == '-' || ch == '*') {
+                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> right = diffWaysToCompute(expression.substring(i + 1));
+                for (int l : left) {
+                    for (int r : right) {
+                        int num = 0;
+                        if (ch == '+') {
+                            num = l + r;
+                        } else if (ch == '-') {
+                            num = l - r;
+                        } else {
+                            num = l * r;
+                        }
+                        res.add(num);
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
---
+++ tree/LC95UniqueBinarySearchTreesII.java
@@ -0,0 +1,41 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC95UniqueBinarySearchTreesII {
+
+    public static void main(String[] args) {
+        System.out.println(generateTrees(2));
+    }
+
+    public static List<TreeNode> generateTrees(int n) {
+        return generateBST(1, n);
+    }
+
+    public static List<TreeNode> generateBST(int left, int right) {
+        List<TreeNode> res = new ArrayList<>();
+
+        if (left > right) {
+            res.add(null);
+            return res;
+        }
+
+        if (left == right) {
+            res.add(new TreeNode(left));
+            return res;
+        }
+
+        for (int i = left; i <= right; i++) {
+            List<TreeNode> leftTrees = generateBST(left, i - 1);
+            List<TreeNode> rightTrees = generateBST(i + 1, right);
+            for (TreeNode leftTree : leftTrees) {
+                for (TreeNode rightTree : rightTrees) {
+                    res.add(new TreeNode(i, leftTree, rightTree));
+                }
+            }
+        }
+
+        return res;
+    }
+}
