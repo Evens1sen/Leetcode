27357e7c38011a43c4c614283172b6d65492ccad
Haoyuan Ma <56732886+Evens1sen@users.noreply.github.com>
2023-03-14 18:10:59 +0800

    Create LC169MajorityElement.java

---
+++ array/LC169MajorityElement.java
@@ -0,0 +1,23 @@
+package array;
+
+class LC169MajorityElement {
+  
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int cnt = 1;
+        for(int num : nums){
+            if(num == candidate){
+                cnt++;
+            }else{
+                cnt--;
+            }
+
+            if(cnt == 0){
+                candidate = num;
+                cnt++;
+            }
+        }
+        
+        return candidate;
+    }
+}
