ff67f5dd5eb2f39be52159fa41a2c08d81d8110f
mhy <11912919@mail.sustech.edu.cn>
2023-03-16 01:08:02 +0800

    Backtracking

---
+++ searching/LC17LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,37 @@
+package searching;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LC17LetterCombinationsOfAPhoneNumber {
+
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("23"));
+    }
+
+    static String[] keysMapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    static List<String> res = new ArrayList<>();
+
+    public static List<String> letterCombinations(String digits) {
+        if (digits.equals("")){
+            return res;
+        }
+
+        backtracking(0, new StringBuilder(), digits);
+        return res;
+    }
+
+    public static void backtracking(int index, StringBuilder path, String dights) {
+        if (index == dights.length()) {
+            res.add(path.toString());
+            return;
+        }
+
+        char[] choices = keysMapping[dights.charAt(index) - '0'].toCharArray();
+        for (char ch : choices) {
+            backtracking(index + 1, path.append(ch), dights);
+            path.deleteCharAt(path.length() - 1);
+        }
+    }
+}
