7ab8a8af0ee53d6249bbca230bf60244386a4d54
mhy <11912919@mail.sustech.edu.cn>
2023-03-17 18:52:13 +0800

    BFS search

--- dynamic/LC279PerfectSquares.java
+++ dynamic/LC279PerfectSquares.java
@@ -1,5 +1,9 @@
 package dynamic;
 
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class LC279PerfectSquares {
 
     public int numSquares(int n) {
@@ -18,5 +22,34 @@ public class LC279PerfectSquares {
         }
 
         return dp[n];
+    }
+
+    public int bfsSolution(int n) {
+        Deque<Integer> queue = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>(); // To reduce calculation
+        queue.add(n);
+        visited.add(n);
+        int depth = 0;
+
+        while (!queue.isEmpty()) {
+            depth++;
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                int cur = queue.poll();
+                for (int j = 1; j * j <= cur; j++) {
+                    int tmp = cur - j * j;
+                    if (tmp == 0) {
+                        return depth;
+                    }
+
+                    if (!visited.contains(tmp)) {
+                        queue.offer(tmp);
+                        visited.add(tmp);
+                    }
+                }
+            }
+        }
+
+        return depth;
     }
 }
