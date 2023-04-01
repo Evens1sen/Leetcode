3e992d85ac2e11b0e654afbdde269555c1bbbcdc
mhy <11912919@mail.sustech.edu.cn>
2023-04-01 16:08:27 +0800

    Knapsack problems

--- dynamic/LC518CoinChange2.java
+++ dynamic/LC518CoinChange2.java
@@ -4,25 +4,33 @@ public class LC518CoinChange2 {
 
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[amount + 1][n + 1];
-        for (int j = 0; j <= n; j++) {
-            dp[0][j] = 1;
+        int[][] dp = new int[n + 1][amount + 1];
+
+        // dp[0][...] = 0
+        // dp[...][0] = 1
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
         }
 
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i - coins[j - 1] < 0) {
+        // dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j]
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j - coins[i - 1] >= 0) {
                     // Pay attention when amount < 0
                     // Avoid index out of bound
-                    dp[i][j] = dp[i][j - 1];
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
                 } else {
                     // Complete knapsack problem:
                     // Coins are not limited, so dp[i][j-coins[i-1]], not dp[i-1][j-coins[i-1]]
-                    dp[i][j] = dp[i][j - 1] + dp[i - coins[j - 1]][j];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
 
-        return dp[amount][n];
+        return dp[n][amount];
     }
+
+    // Follow up: How to calculate the combination ways
+    // Using a matrix G[i][j]: Choose which term at dp[i][j]
+
 }
