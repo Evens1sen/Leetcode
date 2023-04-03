2d13ba5854b014c1f8d749723895a3efb111b3e8
mhy <11912919@mail.sustech.edu.cn>
2023-04-03 19:05:51 +0800

    Bytedance ios client first interview

---
+++ binary/LC33SearchInRotatedSortedArray.java
@@ -0,0 +1,31 @@
+package binary;
+
+public class LC33SearchInRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] >= nums[left]) { // mid on the left side
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // mid on the right side
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
