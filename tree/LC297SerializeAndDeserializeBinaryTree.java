6158fa4478a66f273ef02be08fd6a47aafb30e9b
mhy <11912919@mail.sustech.edu.cn>
2023-04-05 19:41:45 +0800

    Serialize and deserialize trees

--- tree/LC297SerializeAndDeserializeBinaryTree.java
+++ tree/LC297SerializeAndDeserializeBinaryTree.java
@@ -1,19 +1,50 @@
 package tree;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class LC297SerializeAndDeserializeBinaryTree {
 
 }
 
-//TODO
 class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        return null;
+        return recursiveSerialize(root, "");
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        return null;
+        String[] dataArray = data.split(",");
+        List<String> dataList = new LinkedList<String>(Arrays.asList(dataArray));
+        return recursiveDeserialize(dataList);
     }
+
+    public String recursiveSerialize(TreeNode root, String str) {
+        if (root == null) {
+            str += "None,";
+        } else {
+            str += root.val + ",";
+            str = recursiveSerialize(root.left, str);
+            str = recursiveSerialize(root.right, str);
+        }
+        return str;
+    }
+
+    public TreeNode recursiveDeserialize(List<String> dataList) {
+        if (dataList.get(0).equals("None")) {
+            dataList.remove(0);
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(dataList.get(0)));
+        dataList.remove(0);
+        root.left = recursiveDeserialize(dataList);
+        root.right = recursiveDeserialize(dataList);
+
+        return root;
+    }
+
 }
