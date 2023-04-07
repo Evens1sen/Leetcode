51632e84dd201f57672d6d1bcb06b98ad552306b
mhy <11912919@mail.sustech.edu.cn>
2023-04-08 00:02:42 +0800

    Test

--- string/LC415AddStrings.java
+++ string/LC415AddStrings.java
@@ -18,10 +18,8 @@ public class LC415AddStrings {
             int x = (i < 0 ? 0 : num1.charAt(i) - '0');
             int y = (j < 0 ? 0 : num2.charAt(j) - '0');
             int res = x + y + carry;
-            if (res >= 10) {
-                carry = 1;
-                res -= 10;
-            }
+            carry = res / 10;
+            res = res % 10;
             sb.append(res);
             i--;
             j--;
