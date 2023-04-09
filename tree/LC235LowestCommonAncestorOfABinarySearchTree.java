c2cfa0902c7bfaaf95bec6eadc8bbbe23db2630f
mhy <11912919@mail.sustech.edu.cn>
2023-04-09 15:28:07 +0800

    LCA Problems

---
+++ tree/LC235LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class LC235LowestCommonAncestorOfABinarySearchTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
