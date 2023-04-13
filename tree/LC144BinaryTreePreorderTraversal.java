0195490e0c3c8c33ebe4d0ab6a529999f6ab615e
mhy <11912919@mail.sustech.edu.cn>
2023-04-13 15:05:21 +0800

    Stack

---
+++ tree/LC144BinaryTreePreorderTraversal.java
@@ -0,0 +1,46 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LC144BinaryTreePreorderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return iterativeSolution(root);
+    }
+
+    public List<Integer> recursiveSolution(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        res.add(root.val);
+        res.addAll(preorderTraversal(root.left));
+        res.addAll(preorderTraversal(root.right));
+        return res;
+    }
+
+    public List<Integer> iterativeSolution(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            if (cur == null) {
+                continue;
+            }
+            res.add(cur.val);
+            stack.push(cur.right);
+            stack.push(cur.left);
+        }
+
+        return res;
+    }
+}
