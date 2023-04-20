5bb1ba387c8830d7bd89b0010be17a399f9a82f7
mhy <11912919@mail.sustech.edu.cn>
2023-04-20 21:00:01 +0800

    A max heap

---
+++ tree/Heap.java
@@ -0,0 +1,63 @@
+package tree;
+
+public class Heap {
+
+    int size;
+    int[] heap;
+
+    public Heap(int size) {
+        this.size = size;
+        heap = new int[size + 1];
+    }
+
+    public void insert(int val) {
+        size++;
+        heap[size] = val;
+        swim(size);
+    }
+
+    public int deleteMax() {
+        int max = heap[1];
+        swap(1, size);
+        size--;
+        sink(1);
+        return max;
+    }
+
+    public int peek() {
+        return heap[1];
+    }
+
+    public void sink(int i) {
+        while (i * 2 <= size) {
+            int left = i * 2;
+            int right = i * 2 + 1;
+            int largest = i;
+            if (heap[left] >= heap[largest]) {
+                largest = left;
+            }
+            if (right <= size && heap[right] > heap[largest]) {
+                largest = right;
+            }
+            if (largest == i) {
+                break;
+            }
+            swap(i, largest);
+            i = largest;
+        }
+    }
+
+    public void swim(int i) {
+        while (i > 1 && heap[i] > heap[i / 2]) {
+            swap(i, i / 2);
+            i = i / 2;
+        }
+    }
+
+    public void swap(int i, int j) {
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+}
